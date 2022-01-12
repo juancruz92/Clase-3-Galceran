36e1147c77409b2c756975c6e6149b41c4a2d317
Juan Cruz Galceran <g.juancruz@gmail.com>
2022-01-12 18:58:56 -0300

    segundo commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './components/Utilidades/Utilidades.css'
+import './components/NavBar/NavBar.css';
+import Navbar from './components/NavBar/NavBar.js';
+
+function App() {
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+    return <nav className = "navegador">
+        <p className = "brand">BRAND</p>
+            <ul className = "elementosNav">
+                <li><a href = "...">Remeras</a></li>
+                <li><a href = "...">Buzos </a></li>
+                <li><a href = "...">Jeans </a></li>
+                <li><a href = "...">Zaptillas </a></li>
+            </ul>
+    </nav>
+}
+
+export default Navbar;
